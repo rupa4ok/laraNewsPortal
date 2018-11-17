c8b11ff9319ddc3b28d197d82fe3ad785bbc693d
rupak <rupak4@yandex.ru>
2018-11-17 22:41:24 +0300

    Создание авторизации

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>'auth'], function() {
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
