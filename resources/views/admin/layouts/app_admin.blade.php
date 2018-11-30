5216b2bbc03131f0f4a754ed41384d9cf24c1a45
rupak <rupak4@yandex.ru>
2018-11-30 03:03:21 +0300

    Создание новости

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -43,10 +43,8 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                         role="button" aria-expanded="false">Блог</a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Категории</a></li>
-                            <li><a href="#">Материалы</a></li>
-                            <li><a href="#">Категории</a></li>
-                            <li><a href="#">Категории</a></li>
+                            <li><a href="{{route('admin.category.index')}}">Категории</a></li>
+                            <li><a href="{{route('admin.article.index')}}">Новости</a></li>
                         </ul>
                     </li>
                 </ul>
